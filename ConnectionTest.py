e2354c2bf77cf29f62c1ede3aebafd72a6d095e6
Ollie Cockrem <olivercockrem@yahoo.co.uk>
2017-02-25 14:38:34 +0000

    added another page

--- ConnectionTest.py
+++ ConnectionTest.py
@@ -9,70 +9,52 @@ def main():
     conn_string = "host='localhost' dbname='mydb' user='ollie' password='ollie'"
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
-    cursor.execute("SELECT cc_image, x, y, w, h FROM moments WHERE page = 884 AND x > 290 AND y > 580")
+    cursor.execute("SELECT cc_image, x, y, w, h FROM moments WHERE page = 885 AND x > 265 AND y > 550")
     records = cursor.fetchall()
     cursor.close()
 
+
+
     #load all values
     values = []
-    with open('/home/ollie/PycharmProjects/PrimeGenerator/test.txt', 'r') as f:
+    with open('/home/ollie/work/fyp/test.txt', 'r') as f:
             for line in f:
                 values.append(line.split('\n')[0].replace(" ", ""))
                 if 'str' in line:
                     break
 
-    primes = list(values)
+    print(len(values[0]))
+
 
     # load start of lines
     conn_string = "host='localhost' dbname='mydb' user='ollie' password='ollie'"
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
-    cursor.execute("SELECT cc_image FROM moments WHERE page = 884 AND x < 325 AND y > 580 AND y < 5500 ORDER BY y ASC")
+    cursor.execute("SELECT cc_image FROM moments WHERE page = 885 AND x < 300 AND x > 265 AND y > 550 AND y < 5500 ORDER BY y ASC")
     startOfLines = cursor.fetchall()
     cursor.close()
     startOfLinesImg = []
     for c in startOfLines:
         startOfLinesImg.append(c[0])
 
-    #fix broken orders
-    a, b = startOfLinesImg.index('c04229_x0295_y2397.tif'), startOfLinesImg.index('c04266_x0272_y2398.tif')
-    startOfLinesImg[b], startOfLinesImg[a] = startOfLinesImg[a], startOfLinesImg[b]
-    a, b = startOfLinesImg.index('c04450_x0295_y2480.tif'), startOfLinesImg.index('c04468_x0271_y2481.tif')
-    startOfLinesImg[b], startOfLinesImg[a] = startOfLinesImg[a], startOfLinesImg[b]
-    a, b = startOfLinesImg.index('c06766_x0269_y3471.tif'), startOfLinesImg.index('c06760_x0294_y3470.tif')
-    startOfLinesImg[b], startOfLinesImg[a] = startOfLinesImg[a], startOfLinesImg[b]
-    a, b = startOfLinesImg.index('c07862_x0294_y3961.tif'), startOfLinesImg.index('c07885_x0270_y3962.tif')
-    startOfLinesImg[b], startOfLinesImg[a] = startOfLinesImg[a], startOfLinesImg[b]
-    a, b = startOfLinesImg.index('c08782_x0295_y4374.tif'), startOfLinesImg.index('c08820_x0272_y4375.tif')
-    startOfLinesImg[b], startOfLinesImg[a] = startOfLinesImg[a], startOfLinesImg[b]
-    a, b = startOfLinesImg.index('c11137_x0270_y5361.tif'), startOfLinesImg.index('c11117_x0294_y5360.tif')
-    startOfLinesImg[b], startOfLinesImg[a] = startOfLinesImg[a], startOfLinesImg[b]
-
-    indexes = range(9,188,2)
-    for index in sorted(indexes, reverse=True):
-        del startOfLinesImg[index]
-
-    #remove 10 from 100 line
-    startOfLinesImg.remove(startOfLinesImg[99])
-    startOfLinesImg.remove(startOfLinesImg[99])
 
     #checking to see if line starts are accurate
-    #directory = '/home/ollie/PycharmProjects/CharacterSorter/startOfLine'
-    #if not os.path.exists(directory):
-    #    os.makedirs(directory)
-    #else:
-    #    rmtree(directory)
-    #    os.makedirs(directory)
+    directory = '/home/ollie/work/fyp/characterSorter/startOfLine'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    else:
+        rmtree(directory)
+        os.makedirs(directory)
 
-    #i = 0;
-    #for image in startOfLinesImg:
-    #    copyfile('/home/ollie/PycharmProjects/CharacterSorter/page-0884/' + image,
-    #                 '/home/ollie/PycharmProjects/CharacterSorter/startOfLine/' +image)
-    #    i+=1
+    i = 0;
+    for image in startOfLinesImg:
+        copyfile('/home/ollie/PycharmProjects/AandS-mono600_ccs/page-0885/' + image,
+                     '/home/ollie/work/fyp/characterSorter/startOfLine/' +image)
+        i+=1
 
     #create folders
     for i in ['0','1','2','3','4','5','6','7','8','9','all']:
-        directory = '/home/ollie/PycharmProjects/CharacterSorter/' + i
+        directory = '/home/ollie/work/fyp/characterSorter/' + i
         if not os.path.exists(directory):
            os.makedirs(directory)
         else:
@@ -95,14 +77,14 @@ def main():
 
         characters.sort(key=lambda tup: tup[1])
 
-        counter = 0
-        while(counter < len(values[num])):
-            number = values[num][counter]
-            image = characters[counter][0]
-            copyfile('/home/ollie/PycharmProjects/CharacterSorter/page-0884/' + image,
-                 '/home/ollie/PycharmProjects/CharacterSorter/' + number + '/' + image)
+        if len(characters) == 125:
+            counter = 0
+            while(counter < len(values[num])):
+                number = values[num][counter]
+                image = characters[counter][0]
+                copyfile('/home/ollie/PycharmProjects/AandS-mono600_ccs/page-0885/' + image,'/home/ollie/work/fyp/characterSorter/' + number + '/' + image)
 
-            counter += 1
+                counter += 1
 
 if __name__ == "__main__":
     main()
